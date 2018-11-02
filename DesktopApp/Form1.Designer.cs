8ff673fe09a4605fde918e2001ced231df74d100
Kirill <kir.mbr@gmail.com>
2018-11-03 01:59:38 +0300

    Test

---
+++ DesktopApp/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace DesktopApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.LoginBox = new System.Windows.Forms.TextBox();
+            this.PassBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.StatusLable = new System.Windows.Forms.Label();
+            this.Consist = new System.Windows.Forms.Label();
+            this.Debug = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Логин";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LoginBox
+            // 
+            this.LoginBox.Location = new System.Drawing.Point(307, 13);
+            this.LoginBox.Name = "LoginBox";
+            this.LoginBox.Size = new System.Drawing.Size(147, 22);
+            this.LoginBox.TabIndex = 1;
+            // 
+            // PassBox
+            // 
+            this.PassBox.Location = new System.Drawing.Point(307, 42);
+            this.PassBox.Name = "PassBox";
+            this.PassBox.Size = new System.Drawing.Size(147, 22);
+            this.PassBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(190, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(307, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Войти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StatusLable
+            // 
+            this.StatusLable.AutoSize = true;
+            this.StatusLable.Location = new System.Drawing.Point(304, 177);
+            this.StatusLable.Name = "StatusLable";
+            this.StatusLable.Size = new System.Drawing.Size(0, 17);
+            this.StatusLable.TabIndex = 5;
+            // 
+            // Consist
+            // 
+            this.Consist.AutoSize = true;
+            this.Consist.Location = new System.Drawing.Point(742, 421);
+            this.Consist.Name = "Consist";
+            this.Consist.Size = new System.Drawing.Size(0, 17);
+            this.Consist.TabIndex = 6;
+            // 
+            // Debug
+            // 
+            this.Debug.AutoSize = true;
+            this.Debug.Location = new System.Drawing.Point(531, 205);
+            this.Debug.Name = "Debug";
+            this.Debug.Size = new System.Drawing.Size(0, 17);
+            this.Debug.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Debug);
+            this.Controls.Add(this.Consist);
+            this.Controls.Add(this.StatusLable);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PassBox);
+            this.Controls.Add(this.LoginBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox LoginBox;
+        private System.Windows.Forms.TextBox PassBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label StatusLable;
+        private System.Windows.Forms.Label Consist;
+        private System.Windows.Forms.Label Debug;
+    }
+}
+
---
+++ DesktopApp/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DesktopApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        static int port = 1500;
+        static string addres = "185.227.111.201";
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Consist.Text = "No conn";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Debug.Text = "";
+            StatusLable.Text = "";
+
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(addres), port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Connect(ipPoint);
+            if(socket.Connected) Consist.Text = "Connected";
+            string authData = "login " + LoginBox.Text + ";password " + PassBox.Text;
+            byte[] data = Encoding.Unicode.GetBytes(authData);
+            socket.Send(data);
+            if(socket.SendBufferSize > 0)Consist.Text = "Recived";
+
+            data = new byte[256];
+            StringBuilder builder = new StringBuilder();
+            int bytes = 0;
+            do
+            {
+                bytes = socket.Receive(data, data.Length, 0);
+                builder.Append(Encoding.UTF8.GetString(data, 0, bytes));
+            }
+            while (socket.Available > 0);
+            string answer = builder.ToString();
+
+            Debug.Text = answer;
+
+            string[] input = answer.Split(new char[] { ' ' });
+            if(input[0] == "token" && input[1] != "null")
+            {
+                StatusLable.Text = "Вход выполнен";
+                Debug.Visible = false;
+            }
+
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+    }
+}
